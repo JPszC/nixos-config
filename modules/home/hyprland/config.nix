da84b038ad388004c92a7b3e132a8d48ca634c4c
Jaime Pereira <JPszC@pm.me>
2024-11-05 08:03:19 -0400

    Update config.nix

--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -26,7 +26,7 @@
       ];
 
       input = {
-        kb_layout = "us,fr";
+        kb_layout = "latam";
         kb_options ="grp:alt_caps_toggle"; 
         numlock_by_default = true;
         follow_mouse = 0;
